6a3dc14d57e3be3396c23f31ac8563eb06c2c64f
Sara Leal Silverio <saralealsilverio@gmail.com>
2026-02-25 09:53:05 -0700

    adding pages to user management

---
+++ src/components/RegisterForm.svelte
@@ -0,0 +1,18 @@
+
+<form>
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" required />
+
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email" required />
+    
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required />
+
+    <label for="confirm-password">Confirm Password:</label>
+    <input type="password" id="confirm-password" name="confirm-password" required />
+    
+    <button type="submit">Register</button>
+</form>
+
+<p>Already have an account? <a href="login/index.html">Login here</a>.</p>
